81c8f510e4631202c01b0989e059a7b9c901baf9
smyx1 <smyxone@gmail.com>
2020-11-08 22:26:25 -0300

    Settings

--- views.py
+++ views.py
@@ -1,8 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre=nombre
+        self.edad=edad
+        super().__init__()
+
+
 def Saludar(request):
     return HttpResponse("Hola")
 
 def Index(request):
-    return render(request, 'Index.html')
+    amigo=Persona("Andrea Lucero", "25")
+    lista=["Perro", "Gato", "Tortuga"]
+    datos={"Nombre": "Felipe Durán Espinoza", "Animales":lista, "amigo":amigo }
+    return render(request, 'Index.html', datos)
